338346c78259bb3d6e33f37c5e17760a7e698647
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-02-25 20:06:10 +0530

    basic authentication with passport library

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const Path = require("path")
 const methodoverride = require("method-override")
 const ejsMate = require("ejs-Mate");
 const ExpressError = require("./utils/ExpressError.js")       
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+
 const  session = require("express-session")
-const flash = require("connect-flash")
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 main()
 .then(  (res) => {
@@ -50,6 +56,14 @@ app.get("/" , (req,res) =>{
 app.use(session(sessionOptions));
 app.use(flash())                      //use flash before the routes
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use( new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req , res , next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -57,10 +71,20 @@ app.use((req , res , next) => {
     next();    //use next if not wanted to stuck in this middleware
 })
 
+// app.get("/demouser" ,  async (req,res) => {
+//     let fakeUser = new User({
+//         email : "student@gmail.com",
+//         username : "sigma-student"
+//     });
 
+//     let registerUser = await User.register(fakeUser , "helloworld");
 
-app.use("/listings" , listings)
-app.use("/listings/:id/reviews" , reviews)
+//     res.send(registerUser)
+// })
+
+app.use("/listings" , listingRouter)
+app.use("/listings/:id/reviews" , reviewRouter)
+app.use("/" , userRouter)
 
 
 // app.get("/testlisting" , async (req,res) => {
